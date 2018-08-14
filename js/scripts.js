2b05f1707d766e06744f8fd8dffad171ae61db8e
Kendra Kelly and Victoria Martinez <kendra@>
2018-08-14 15:24:14 -0700

    tesing out the loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,42 @@
 $(document).ready(function() {
-
   $(".formInput").submit(function(event) {
-
-    var visitorInputs = $("#inputSentence").val();
     event.preventDefault();
-
+    var inputSentence = $("#inputSentence").val();
     var vowels = ["a", "e", "i", "o", "u"];
 
-    var inputReplace1 = visitorInputs.replace(/a/gi , "_");
-    var inputReplace2 = inputReplace1.replace(/e/gi , "_");
-    var inputReplace3 = inputReplace2.replace(/i/gi , "_");
-    var inputReplace4 = inputReplace3.replace(/o/gi , "_");
-    var finalReplace = inputReplace4.replace(/u/gi , "_");
-debugger;
-    // $("#formInput").submit(function() {
-      $("#final").text(finalReplace);
+    var endingSentence = words.join("_");
+    var splitValue = vowels[i];
+
+    for (var i = 0; i < inputSentence.length; i++) {
+
+      console.log(splitValue);
+      var words = inputSentence.split(splitValue);
+      endingSentence.split(splitValue);
+      // console.log(words);
+      //
+      // console.log(endingSentence);
+
+
+    };
+
+
+
+    console.log('FINAL VALUE');
+    console.log(endingSentence);
+    $("#final").text(endingSentence);
+
+
+
+
+
+//     var inputReplace1 = inputSentence.replace(/a/gi , "_");
+//     var inputReplace2 = inputReplace1.replace(/e/gi , "_");
+//     var inputReplace3 = inputReplace2.replace(/i/gi , "_");
+//     var inputReplace4 = inputReplace3.replace(/o/gi , "_");
+//     var finalReplace = inputReplace4.replace(/u/gi , "_");
+// debugger;
+//     // $("#formInput").submit(function() {
+
 
   });
 });
